b77788bf40cd9077fbaed223edede4ac28328893
DEVINKoff <161931008+DEVINKoff@users.noreply.github.com>
2025-12-26 18:17:15 +0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,49 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        styles: './src/styles/main.scss'
-    },
-    output: {
-        clean: false,
-        assetModuleFilename: "assets/[hash][ext]"
-    },
-    devServer: {
-        open: true,
-        static: {
-            directory: './src',
-            watch: true
-        }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({template: "./src/index.html"})
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
+  entry: {
+    styles: "./src/styles/main.scss"
+  },
+  mode: "development",
+  output: {
+    assetModuleFilename: "assets/[hash][ext]",
+    clean: false
+  },
+  devServer: {
+    open: true,
+    static: {
+      watch: true,
+      directory: "./src"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
